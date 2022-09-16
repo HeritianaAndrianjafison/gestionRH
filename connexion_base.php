fff718f15f0ed091609e1d6da83ed0c15230f1f2
tix200 <antema200@gmail.com>
2022-09-16 12:23:21 +0300

    first commit

---
+++ connexion_base.php
@@ -0,0 +1,6 @@
+ <?php 
+ 	$servername = 'mysql-tixdevs.alwaysdata.net';
+    $username = 'tixdevs';
+    $password = 'tina0586';
+    $conn = new PDO("mysql:host=$servername;dbname=tixdevs_base", $username, $password);
+  ?>
